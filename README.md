2c866e21e128badfcd47044348884a9ee1e6f420
bett-bett <kipbryan.bett@gmail.com>
2025-02-15 12:52:16 +0300

    sum, Generator expression

--- README.md
+++ README.md
@@ -59,3 +59,12 @@ Constraints:
 
 ## [fizzbuzz2.py](python/fizzbuzz2.py)
 list comprehension
+
+## [fizzbuzz3-sum.py](python/fizzbuzz3-sum.py)
+Generator expression
+
+[problem source](https://projecteuler.net/problem=1)
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+Find the sum of all multiples of 3 and 5 below x
+
+  | Sum of fizz buzz 
---
+++ python/fizzbuzz3-sum.py
@@ -0,0 +1,22 @@
+def sum_fizz_buzz (number):
+  # simple solution
+  # sum=0
+  # for i in range (number):
+  #   if(i%5==0 or i%3==0):
+  #     sum+=i
+  # return sum
+
+  # sum is a built in python function, fyi
+
+  # Of interest today is generator expression, look at list comprehension as well
+  # Generator expression (expression for item in iterable)
+  # create list with a shorter syntax. elements are generated when they are needed, meaning the full lust is not loaded in memory as opposed to list comprehension 
+  return sum(
+    i for i in range(number) if (i%3==0 or i%5==0)
+  )
+
+  # list comprehension
+  # return sum([i for i in range(number) if (i % 3 == 0 or i % 5 == 0)])
+
+
+print(sum_fizz_buzz(10))
